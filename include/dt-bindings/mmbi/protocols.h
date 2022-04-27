79d9e600f5a2c0e3723142db004bccedc3957096
Priyanka Chikane <priyanka.chikane@intel.com>
2022-04-27 09:08:24 +0000

    dt-bindings: mmbi: Add mmbi protocols in DTS
    This commit adds MMBI dynamic protocols support using aspeed DTS. The
    MMBI supported protocols are added in the aspeed DTS. The driver soc can
    get the supported protocols from DTS file.
    
    Tested:
    - Two devices(seamless & ras_offload) shows up as expected.
    - MMBI channel init fails if no. of supported protocol are not in
    desired range or if no instance found in DTS.
    
    Signed-off-by: Priyanka Chikane <priyanka.chikane@intel.com>

---
+++ include/dt-bindings/mmbi/protocols.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (c) 2022, Intel Corporation. */
+
+#ifndef _DT_BINDINGS_MMBI_PROTOCOLS_H_
+#define _DT_BINDINGS_MMBI_PROTOCOLS_H_
+
+//This definitions are as per MMBI specification.
+#define MMBI_PROTOCOL_IPMI 1
+#define MMBI_PROTOCOL_SEAMLESS 2
+#define MMBI_PROTOCOL_RAS_OFFLOAD 3
+#define MMBI_PROTOCOL_MCTP 4
+#define MMBI_PROTOCOL_NODE_MANAGER 5
+
+#endif
